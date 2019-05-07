8d3bf654f28533f33a53944dd4d03d93990ce3de
kiven <984768569@qq.com>
2019-05-07 10:52:28 +0800

    *

--- circleindicator/src/main/java/me/relex/circleindicator/CircleIndicator.java
+++ circleindicator/src/main/java/me/relex/circleindicator/CircleIndicator.java
@@ -7,16 +7,16 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.database.DataSetObserver;
 import android.os.Build;
-import android.support.annotation.AnimatorRes;
-import android.support.annotation.DrawableRes;
-import android.support.v4.view.ViewPager;
+import androidx.annotation.AnimatorRes;
+import androidx.annotation.DrawableRes;
+import androidx.viewpager.widget.ViewPager;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.view.animation.Interpolator;
 import android.widget.LinearLayout;
 
-import static android.support.v4.view.ViewPager.OnPageChangeListener;
+import static androidx.viewpager.widget.ViewPager.OnPageChangeListener;
 
 public class CircleIndicator extends LinearLayout {
 
